aaea4c4fb60623bc4058d18d12de9d4908c89f0a
Manon555 <70019430+Manon555@users.noreply.github.com>
2025-12-04 09:36:35 +0100

    Add print statement for example plot in demo.R

--- demo.R
+++ demo.R
@@ -2,5 +2,6 @@ find.package("tidyverse")
 library(tidyverse)
 data(mtcars)
 mycars <- mtcars %>% select(mpg,cyl)
+print("an example plot")
 ggplot(mycars,aes(cyl,mpg)) +
   geom_point(colour="red",size=3)
